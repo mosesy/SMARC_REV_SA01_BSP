030f9f7c4164deb9be254eeb6e02670428f2eafd
Nick Liu <nick_liu@avalue.com.tw>
2016-07-07 19:59:14 +0800

    Modified DTS's Makefile and kernel's run.sh.
    modified:   kernel/arch/arm/boot/dts/Makefile
    modified:   kernel/run.sh

--- kernel/run.sh
+++ kernel/run.sh
@@ -1,30 +1,39 @@
 #!/bin/bash
 
-LOCAL_PATH=`pwd`
 ARCH=arm
-CROSS_COMPILE=$LOCAL_PATH/../fsl/bin/arm-fsl-linux-gnueabi-
+CROSS_COMPILE=../fsl/bin/arm-fsl-linux-gnueabi-
 JOBS=$1
 
 if [ $1 == "distclean" ]; then
-	make ARCH=$ARCH CROSS_COMPILE=$CROSS_COMPILE distclean
-	exit;
+    make ARCH=$ARCH CROSS_COMPILE=$CROSS_COMPILE distclean
+    exit;
 fi
 
 if [ $1 == "clean" ]; then
-	make ARCH=$ARCH CROSS_COMPILE=$CROSS_COMPILE clean
-	exit;
+    make ARCH=$ARCH CROSS_COMPILE=$CROSS_COMPILE clean
+    exit;
 fi
 
 if [ $1 == "menuconfig" ]; then
-	make ARCH=$ARCH CROSS_COMPILE=$CROSS_COMPILE $1
-	exit;
+    make ARCH=$ARCH CROSS_COMPILE=$CROSS_COMPILE $1
+    exit;
 fi
 
 if [ $1 == "xconfig" ]; then
-	make ARCH=$ARCH CROSS_COMPILE=$CROSS_COMPILE $1
-	exit;
+    make ARCH=$ARCH CROSS_COMPILE=$CROSS_COMPILE $1
+    exit;
 fi
 
 make ARCH=$ARCH CROSS_COMPILE=$CROSS_COMPILE imx_v7_defconfig
 make ARCH=$ARCH CROSS_COMPILE=$CROSS_COMPILE $1
+
+if [ ! -d ../out ]; then
+    mkdir ../out
+fi
+
+install arch/arm/boot/zImage                                ../out/zImage
+install arch/arm/boot/dts/imx6q-rev-sa01-pfuze100.dtb       ../out/imx6q-rev-sa01-pfuze100.dtb
+install arch/arm/boot/dts/imx6solo-rev-sa01-pfuze100.dtb    ../out/imx6solo-rev-sa01-pfuze100.dtb
+install arch/arm/boot/dts/imx6q-rev-sa01-wm8326.dtb         ../out/imx6q-rev-sa01-wm8326.dtb
+install arch/arm/boot/dts/imx6solo-rev-sa01-wm8326.dtb      ../out/imx6solo-rev-sa01-wm8326.dtb
 
